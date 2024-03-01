7a421bd295925545f64ddc974b4e46c066cb53e1
Khalid6468 <khalimohammed5@gmail.com>
2024-03-02 00:34:45 +0530

    Added healthInfra API

--- infra/main.go
+++ infra/main.go
@@ -5,13 +5,34 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/SystemsStuff/ShrinkSync/infra/handlers"
+	"github.com/SystemsStuff/ShrinkSync/infra/utils"
 )
 
 func main() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path[1:]))
-    })
+	go func() {
+		for {
+			log.Printf("Heartbeat check...")
+			response := utils.GetNodesStatus()
+			nodes := utils.GetNodes()
+			for _, node := range nodes {
+				if response.StatusJson[node] != "UP" {
+					// take appropriate action when the node is down
+					log.Printf("node %s is DOWN...\n", node)
+				}
+			}
+			time.Sleep(1 * time.Minute)
+		}
+	}()
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path[1:]))
+	})
+	mux.HandleFunc("/infra-health", handlers.InfraHealthHandler)
+
+	log.Fatal(http.ListenAndServe(":9090", mux))
 }
--- infra/utils/utils.go
+++ infra/utils/utils.go
@@ -3,8 +3,34 @@ package utils
 import (
 	"log"
 	"os"
+	"os/exec"
 	"strconv"
 )
+
+type NodesStatus struct {
+	StatusJson map[string]string `json:"nodes_status"`
+}
+
+func PingNode(node string) error {
+	cmd := exec.Command("ping", "-w", "2", node)
+	err := cmd.Run()
+	return err
+}
+
+func GetNodesStatus() *NodesStatus {
+	nodes := GetNodes()
+	response := &NodesStatus{
+		StatusJson: make(map[string]string),
+	}
+	for _, node := range nodes {
+		if err := PingNode(node); err != nil {
+			response.StatusJson[node] = "DOWN"
+		} else {
+			response.StatusJson[node] = "UP"
+		}
+	}
+	return response
+}
 
 func GetNodes() []string {
 	mapNodes, _ := strconv.ParseInt(os.Getenv("MAP_NODE_COUNT"), 10, 64)
